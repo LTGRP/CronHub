df4d75ffc68cdec8f5ca74d8b34212978aa62420
LTGRP Heavy Industries <66345220+LTGRP@users.noreply.github.com>
2020-07-15 09:41:13 -0400

    Update PageActionUtils.java

--- src/org/cronhub/managesystem/commons/action/PageActionUtils.java
+++ src/org/cronhub/managesystem/commons/action/PageActionUtils.java
@@ -6,7 +6,7 @@ import org.apache.struts2.ServletActionContext;
 import org.cronhub.managesystem.commons.params.Params;
 
 /***
- * 这个类主要用于将实际数据List,总共多少页,翻页后应该到第几页了,总共多少条记录等,页面上要用的放入Request，传回给页面,供展示用的类,没有涉及分页算法的计算
+ * 
  * @author dd
  *
  */
